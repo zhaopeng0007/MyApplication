1a455e6907ef71c316f0cc9a43e03f5a80b03593
zhaopeng08 <zhaopeng08@baidu.com>
2016-12-07 22:49:33 +0800

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/testzhaopeng.java
@@ -0,0 +1,15 @@
+package com.example.mylibrary;
+
+import android.provider.Settings;
+
+/**
+ * Created by zhaopeng08 on 2016/12/7.
+ */
+
+public class testzhaopeng {
+    public  static  void  main(){
+
+        System.out.print("dfasdfasd");
+
+    }
+}
